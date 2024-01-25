68de0f5d70462df226eb5718c60678a04a0d8ffc
anita <anita8004@gmail.com>
2024-01-25 18:33:01 +0800

    feature: login

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import * as bootstrap from 'bootstrap'
 import './scss/style.scss'
 import App from './App.vue'
 import router from './router'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
 
 const app = createApp(App)
 app.use(router)
+// app.use(VueAxios, axios)
 app.mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container mt-5">
-  <form class="row justify-content-center">
+  <form class="row justify-content-center" @submit.prevent="login">
     <div class="col-md-6">
       <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
       <div class="mb-2">
@@ -12,6 +12,7 @@
           placeholder="Email address"
           required
           autofocus
+          v-model="state.username"
         />
       </div>
       <div class="mb-2">
@@ -22,6 +23,7 @@
           class="form-control"
           placeholder="Password"
           required
+          v-model="state.password"
         />
       </div>
   
@@ -33,10 +35,36 @@
 </div>
 </template>
 
-<script>
-  export default {
-    
-  }
+<script setup>
+import { reactive } from 'vue';
+import axios from 'axios';
+import router from '../router';
+import Cookies from 'js-cookie';
+
+const state = reactive({
+  username: '',
+  password: ''
+})
+
+const login = () => {
+  const url = import.meta.env.VITE_APP_API;
+  const api = `${url}/admin/signin`;
+  axios.post(api, state)
+  .then(res => {
+    console.log(res);
+    console.log(res.data.success);
+    if (res.data.success) {
+      const { token, expired } = res.data;
+      // document.cookie = `hexToken=${token};expires=${new Date(expired)};`;
+      Cookies.set('hexToken', token, { expires: new Date(expired) })
+      router.push({ name: 'home' });
+    }
+  })
+  .catch(err => {
+    console.error(err)
+  })
+}
+
 </script>
 
 <style lang="scss" scoped>
